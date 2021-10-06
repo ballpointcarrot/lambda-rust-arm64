5d9ac66a0dcdd1efdf427d55b09cb91fb0c707f0
Christopher Kruse <ckruse@ballpointcarrot.net>
2021-10-06 15:21:11 -0700

    add packaging script for easy use.

---
+++ package.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p dist
+
+if [ ! -d target/aarch64-unknown-linux-musl ]; then
+  ./musl-build cargo build --release
+fi
+
+# TODO: use vars for arch folder and binary name
+cp target/aarch64-unknown-linux-musl/release/bootstrap dist/
+cd dist && zip package.zip bootstrap && cd ..
+
